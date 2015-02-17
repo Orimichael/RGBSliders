cfd9ba3035e75e2b874a4ef3f5896e0394afcf0f
Ori Benhamou <ori@benhamou.com>
2015-02-17 14:41:16 -0500

    RGB sliders now control a square view and can jump to/from a 2nd ViewController through Navigation Bar

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    
+    @IBOutlet weak var squareView: UIView!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBAction func resetButton(sender: UIButton) {
+        redSlider.value = 0.5
+        greenSlider.value = 0.5
+        blueSlider.value = 0.5
+        updateSquareColor()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        squareView.layer.borderColor = UIColor.blackColor().CGColor
+        squareView.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func updateSquareColor() {
+        // sets background color to colors established by sliders
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        squareView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
 
